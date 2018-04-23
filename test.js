3dca4100ad4d56731b8157051d69af640f8a9960
Glenn Craver <glenn.craver@rockfordcorp.com>
2018-04-23 16:43:07 -0700

    reworked test.js to test importing and using the library

--- test.js
+++ test.js
@@ -1,28 +1,7 @@
-class MyArray extends Array {
+const Vector = require('./src/index').Vector
 
-  forEach(func) {
-    Array.prototype.forEach.call(this, func)
-  }
-
-  static sayHi(name) { console.log('hello', name)}
-
-  sayHi() { console.log('non-static hello', arguments[0] ? arguments[0] : 'world!')}
-
-}
-
-let a = new MyArray()
-
-a.sayHi()
-
-MyArray.sayHi()
-
-
-// let a = new MyArray(1,2,3)
-
-// console.log('index 0',a[0],'index -1', a[-1])
-// Array.prototype.forEach.call(a, (val, ind) => console.log('proto:',ind, val))
-// a.push('apple')
-// a.shift()
-// a.unshift('wonton')
-// a.forEach((val,ind)=>console.log('mine',ind,val))
-// console.log('length',a.length)
+let v = new Vector(1.1,2.2,3.3)
+console.log(v)
+console.log(v.add(v))
+console.log(v.magnitude(v))
+console.log(v.dot(v)) // v·v should be ||v|| squared
