df855e1ce073612b017f4d8e52e7832dbec7da15
Glenn Craver <glenn.craver@rockfordcorp.com>
2018-05-07 15:51:46 -0700

    playing around with 'generator' functions

--- test.js
+++ test.js
@@ -5,3 +5,19 @@ console.log(v)
 console.log(v.add(v))
 console.log(v.magnitude(v))
 console.log(v.dot(v)) // v·v should be ||v|| squared
+
+function* getNextNumber() {
+  let num = 0;
+  while(num < 5) {
+    yield num;
+    num++
+  }
+}
+
+myIter = getNextNumber()
+
+console.log(myIter.next(), myIter.next(), myIter.next(), myIter.next(), myIter.next(), myIter.next())
+
+for(const val of getNextNumber()) {
+  console.log('Iterating...', val)
+}
